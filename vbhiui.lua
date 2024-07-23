5e2acade64fd56f6f0abc1afad66c2010c88da13
dingding123hhh <168184738+dingding123hhh@users.noreply.github.com>
2024-07-23 18:24:40 +0800

    Add files via upload

--- vbhiui.lua
+++ vbhiui.lua
@@ -223,7 +223,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "rbxassetid://8932031696"
+      DropShadow.Image = "rbxassetid://16235057072"
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
